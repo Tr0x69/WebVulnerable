b5049d918ee9bffba60ba3a6dffecebf9854cdda
19418 <doanminhtri1443@gmail.com>
2024-12-04 18:35:21 -0600

    Test Api

---
+++ WebVulnerable/Controllers/VillaApiController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebVulnerable.Data;
+using WebVulnerable.Data.Dto;
+
+namespace WebVulnerable.Controllers
+{
+    [Route("api/villa")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDto>> GetVilla()
+        {
+            return Ok(VillaStore.villaList);
+
+        }
+
+        [HttpGet("{id:int}", Name ="GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]//provide documented in swagger
+        public ActionResult<VillaDto> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+            if (villa == null) {
+                return NotFound();
+            }
+            return Ok(villa);
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDto> CreateVilla([FromBody]VillaDto villaDto)
+        {
+            if (villaDto == null)
+            {
+                return BadRequest(villaDto);
+            }
+
+            if(villaDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDto.Id = VillaStore.villaList.OrderByDescending(u=>u.Id).FirstOrDefault().Id+1;
+            VillaStore.villaList.Add(villaDto);
+
+            return CreatedAtRoute(nameof(GetVilla), new {id = villaDto.Id } ,villaDto);
+        }
+
+
+    }
+}
---
+++ WebVulnerable/Models/Villa.cs
@@ -0,0 +1,11 @@
+﻿namespace WebVulnerable.Data
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate {  get; set; }
+
+
+    }
+}
